8e0a304572e1c51afa3327847c07581de7672196
Jd1ego <juancmo237@gmail.com>
2024-09-14 12:05:44 -0500

    Primer commit

---
+++ Back/cine_back/src/main/java/com/jcja/cine_back/CineBackApplication.java
@@ -0,0 +1,13 @@
+package com.jcja.cine_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBackApplication.class, args);
+	}
+
+}
---
+++ Back/cine_back/src/test/java/com/jcja/cine_back/CineBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcja.cine_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
